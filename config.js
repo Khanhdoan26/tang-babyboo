f1f7faa5f89e6d5d89ba793924f9cf5552779c09
Khanhdoan26 <90522956+Khanhdoan26@users.noreply.github.com>
2021-09-12 00:22:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'alo',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao anh cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    introDesc: `thấy hay quó nên làm gửi anh nahhhh`,
+    btnIntro: 'bấm zô',
     title: 'do con cho (nhma la cua em <3) 😙',
-    desc: 'mong anh luôn vui vẻ và hạnh phúc, yêu em nữaaaaa',
-    btnYes: 'thích anh nhìu',
-    btnNo: 'iu nhất trên đời',
+    desc: 'mong anh luôn mạnh khỏe và hạnh phúc, yêu em nữaaaaa',
+    btnYes: 'anh yêu em bé nhìu nhắmmmm',
+    btnNo: 'khum iu khum iu',
     question:'đừng có tự nhiên cãi nhau vì mấy cái đâu đâu nữa nha trời',
-    btnReply: 'nói chung nà iu nhìu úm',
-    reply: 'thấy ghét',
+    btnReply: 'anh biết oy hong z nữa anh iu em nhìu',
+    reply: 'love u',
     mess: 'baby boo cua emmmm',
-    messDesc: 'iu em diiii',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'iu em hong',
+    btnAccept: 'iu nhieu nham <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
