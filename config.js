ec8e24e592d2c30aacdb95e3ce13f3acdd894616
Khanhdoan26 <90522956+Khanhdoan26@users.noreply.github.com>
2021-09-12 00:27:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     btnNo: 'khum iu khum iu',
     question:'đừng có tự nhiên cãi nhau vì mấy cái đâu đâu nữa nha trời',
     btnReply: 'bấm típ ih',
-    reply: 'anh biết oy hong z nữa anh iu em nhìu',
+    reply: 'anh biết oy hong z nữa anh iu em nhìuu',
     mess: 'baby boo cua emmmm',
     messDesc: 'có iu em hongggg',
     btnAccept: 'cooooo',
